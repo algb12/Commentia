358f28c8c61c42bb3a3a5fdfdc9f5eb092dcf7d5
Alexander Gilburg (algb12) <alexander.gilburg@gmail.com>
2016-07-16 00:32:19 +0200

    Changed default locale back to en_US (American English)

--- app/data/config.php
+++ app/data/config.php
@@ -1,18 +1,18 @@
 <?php
 
-# Commentia config
-# These are the settings to be adjusted by the webmaster.
-# Member role settings can be found in the MembersRoles.php file
-# Author: Alexander Gilburg
-# Last updated: 15th of July 2016
+// Commentia config
+// These are the settings to be adjusted by the webmaster.
+// Member role settings can be found in the MembersRoles.php file
+// Author: Alexander Gilburg
+// Last updated: 15th of July 2016
 
-# Locale for language to be used by lexicon
-# English (USA): en_US
-# German (Germany): de_DE
-define('LEX_LOCALE', 'fr_FR');
+// Locale for language to be used by lexicon
+// English (USA): en_US
+// German (Germany): de_DE
+define('LEX_LOCALE', 'en_US');
 
-# Relative path to JSON file containing comments
+// Relative path to JSON file containing comments
 define('JSON_FILE_COMMENTS', 'app/data/comments.example.json');
 
-# Relative path to JSON file containing members
+// Relative path to JSON file containing members
 define('JSON_FILE_MEMBERS', 'app/data/members.example.json');
