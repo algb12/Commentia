af32401ea9309c7b0b9db3d30840b7c84110f0f5
Alexander Gilburg (algb12) <alexander.gilburg@gmail.com>
2016-07-16 21:46:52 +0200

    Updated locale to use '-' instead of '_' to adhere to RFC5646

---
+++ app/Commentia/Lexicon/locale/fr-FR.php
@@ -0,0 +1,28 @@
+<?php
+
+// French lexicon (France)/Lexique français (France)
+// Format: define('PHRASE_NAME', 'localized phrase');
+// Don't forget to escape the single quotes (') using a backslash, such as 'J\'ai' instead of 'J'ai'
+// Author: Alexander Gilburg
+// Last updated: 15th of July 2016
+
+define('LANG_NAME', 'French (France)');
+define('LANG_NAME_LOCALIZED', 'Français (France)');
+
+define('TITLES_NEW_COMMENT', 'Nouveau commentaire:');
+
+define('COMMENT_INFO_COMMENT_BY', 'Commentaire de:');
+define('COMMENT_INFO_POSTED_AT', 'Publié:');
+
+define('COMMENT_CONTROLS_REPLY', 'répondre');
+define('COMMENT_CONTROLS_EDIT', 'modifier');
+define('COMMENT_CONTROLS_DELETE', 'supprimer');
+define('COMMENT_CONTROLS_PUBLISH', 'publier');
+
+define('DIALOGS_DELETE_COMMENT', 'Êtes-vous sûr que vous voulez supprimer le commentaire?');
+
+define('AUTH_FORM_BUTTONS_LOG_IN', 'Connexion');
+define('AUTH_FORM_BUTTONS_LOG_OUT', 'Déconnexion');
+
+define('AUTH_FORM_LABELS_USERNAME', 'Nom d\'utilisateur:');
+define('AUTH_FORM_LABELS_PASSWORD', 'Mot de passe:');
