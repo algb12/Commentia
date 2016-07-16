f7740a46587facd5c578f958429f92d215b1a492
Alexander Gilburg (algb12) <alexander.gilburg@gmail.com>
2016-07-16 20:57:18 +0200

    Moved from arrays to constants method for i18n

--- app/Commentia/Lexicon/Lexicon.php
+++ app/Commentia/Lexicon/Lexicon.php
@@ -1,11 +1,11 @@
 <?php
 
-# Commentia phrase lexica
-# Format: $phrases['locale']['category_name']['phrase_name'] = 'localized_phrase';
-# Don't forget to escape the single quotes (') using a backslash, such as 'J\'ai' instead of 'J'ai'
-# Call constructor of Lexicon class with locale as argument, then get phrase with getPhrase($category, $object)
-# Author: Alexander Gilburg
-# Last updated: 15th of July 2016
+// Commentia phrase lexica
+// Format: $phrases['locale']['category_name']['phrase_name'] = 'localized_phrase';
+// Don't forget to escape the single quotes (') using a backslash, such as 'J\'ai' instead of 'J'ai'
+// Call constructor of Lexicon class with locale as argument, then get phrase with getPhrase($category, $object)
+// Author: Alexander Gilburg
+// Last updated: 15th of July 2016
 
 namespace Commentia\Lexicon;
 
@@ -14,5 +14,10 @@ class Lexicon
     public static function load($locale)
     {
         require __DIR__.'/locale/'.$locale.'.php';
+    }
+
+    public static function getPhrase($phrase)
+    {
+        return constant($phrase) ? constant($phrase) : 'Error: Undefined phrase in lexicon '."'$phrase'";
     }
 }
