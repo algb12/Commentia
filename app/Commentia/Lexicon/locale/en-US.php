8042f45d6189e6129a03b524007575cbdb499a2f
Alexander Gilburg (algb12) <alexander.gilburg@gmail.com>
2016-07-16 21:46:38 +0200

    Updated locale to use '-' instead of '_' to adhere to RFC5646

---
+++ app/Commentia/Lexicon/locale/en-US.php
@@ -0,0 +1,28 @@
+<?php
+
+// American English lexicon/American English lexicon
+// Format: define('PHRASE_NAME', 'localized phrase');
+// Don't forget to escape the single quotes (') using a backslash, such as 'J\'ai' instead of 'J'ai'
+// Author: Alexander Gilburg
+// Last updated: 15th of July 2016
+
+define('LANG_NAME', 'English (USA)');
+define('LANG_NAME_LOCALIZED', 'English (USA)');
+
+define('TITLES_NEW_COMMENT', 'New comment');
+
+define('COMMENT_INFO_COMMENT_BY', 'Comment by:');
+define('COMMENT_INFO_POSTED_AT', 'Posted:');
+
+define('COMMENT_CONTROLS_REPLY', 'reply');
+define('COMMENT_CONTROLS_EDIT', 'edit');
+define('COMMENT_CONTROLS_DELETE', 'delete');
+define('COMMENT_CONTROLS_PUBLISH', 'publish');
+
+define('DIALOGS_DELETE_COMMENT', 'Are you sure that you want to delete this comment?');
+
+define('AUTH_FORM_BUTTONS_LOG_IN', 'Log in');
+define('AUTH_FORM_BUTTONS_LOG_OUT', 'Log out');
+
+define('AUTH_FORM_LABELS_USERNAME', 'Username:');
+define('AUTH_FORM_LABELS_PASSWORD', 'Password:');
